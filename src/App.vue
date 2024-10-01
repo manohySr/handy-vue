a43449040672926eced9ec0915ec2c511ea9774a
manohySr <manohyrajaonah@gmail.com>
2024-10-01 19:25:39 +0400

    add: option api

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: true,
+      tasks: ['Task One', 'Task two', 'Task three'],
+      link: 'https://www.google.com'
+    }
+  },
+  methods: {
+    toggleStatus() {
+      this.status = !this.status
+    }
+  }
+}
+</script>
+<template>
+  <h1>Hello {{ name }}</h1>
+  <p v-if="status">you are active</p>
+  <p v-else>you are inactive</p>
+  <a :href="link">Look for me</a>
+  <br />
+  <br />
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <br />
+  <button @click="toggleStatus">LogIn/Out</button>
+</template>
