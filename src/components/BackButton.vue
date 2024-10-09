9820b2f59d22cfc667d1154fb8af006b4a8a5d5b
manohySr <manohyrajaonah@gmail.com>
2024-10-09 11:53:42 +0400

    style:	remove some margin to make it cooler

--- src/components/BackButton.vue
+++ src/components/BackButton.vue
@@ -6,8 +6,8 @@ import { RouterLink } from 'vue-router'
   <section>
     <div class="container m-auto py-6 px-6">
       <RouterLink to="/jobs" class="text-green-500 hover:text-green-600 flex items-center">
-        <i class="pi pi-arrow-circle-left mr-3"></i>
-        <p class="ml-3">Back to Job Listings</p>
+        <i class="pi pi-arrow-circle-left mr-1"></i>
+        <p class="ml-1">Back to Job Listings</p>
       </RouterLink>
     </div>
   </section>
